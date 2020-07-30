01033b4aba2e4122aba53e233f05e23ffd2185aa
Rajesh336 <68871163+Rajesh336@users.noreply.github.com>
2020-07-30 16:36:57 +0530

    Add files via upload

---
+++ task_multiple input.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,num3,sum=0;
+	printf("enter the three number");
+	scanf("%d %d %d",&num1,&num2,&num3);
+	sum=num1+num2+num3;
+	printf("%d+%d+%d=%d",num1,num2,num3,sum);
+}
